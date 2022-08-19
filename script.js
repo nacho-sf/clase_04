97848b3d859f212e67f1b9de9843309e0b687c63
Nacho-sf <serranof.ignacio@gmail.com>
2022-08-19 11:53:25 +0200

    sin terminar último

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// https://lenguajejs.com/javascript/asincronia/que-es/
 // En el siguiente código se tiene un retardo simulado (de 3 seg) para probar las promesas (en este ejemplo el retardo sería síncrono)
 
 
@@ -13,6 +13,7 @@ let myPromise = new Promise(function(resolve, reject) {
   });
   
   console.log(myPromise);
+  console.log(typeof myPromise);
 
 
   // El código de la promesa está definido en las líneas 6, 7 y 8. Es un 'timer' que contiene una función que escupe un string: 'Te saludo tras 3 segundos!! '
@@ -184,6 +185,195 @@ fetch("http://httpstat.us/500")
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+////// TEORÍA DE ASINCRONÍA CALLBACKS Y PROMISE /////////
+
+
+/*
+const iterations = 10;
+const numbers = [];
+
+for (let i = 0; i < iterations; i++) {
+  const number = 1 + Math.floor(Math.random() * 6);
+  numbers.push(number);
+  if (number === 6) {
+    console.log("ERROR");
+    break;
+  }
+}
+
+console.log(numbers);
+*/
+
+
+
+
+/*
+console.log("");
+// Ejemplo sin función Callback
+
+const list = ["A", "B", "C"];
+
+for (let i = 0; i < list.length; i++) {
+  console.log("i=", i, " list=", list[i]);
+}
+*/
+
+
+
+/*
+console.log("");
+// Ejemplo con función Callback
+
+list.forEach(function(currentValue, index) {
+    console.log("i=", index, " list=", currentValue);
+  });
+*/
+
+
+
+
+/*
+console.log("");
+// setTimeout(Callback 'function', time 'number'):
+
+setTimeout(function() {
+    //console.log("He ejecutado la función");
+  }, 2000);
+*/
+
+
+
+
+/////////////// ASINCRONÍA CON CALLBACKS ///////////////
+
+// Se declara la función primaria como la variable 'doTask'.
+
+// Se le pasa por parámetros:
+    // Nº iteraciones para el bucle for (tiradas de dados)
+    // Nombre de la función Callback
+
+// El código de la función primaria consiste en un bucle 'for', el cual, en cada iteración genera un número entero aleatorio del 1 al 6 y lo guarda en el array 'numbers'
+
+// Si el número generado es 6:
+    // Devuelve el parámetro/función 'Callback' con su primer argumento 'err' (Objeto error)
+
+// Si tras completar las iteraciones, no se saca 6:
+    // Devuelve el parámetro/función 'Callback' con su segundo argumento 'result' (Objeto resultado)
+
+/*
+const doTask = (iterations, miCallback) => {
+    const numbers = [];
+    for (let i = 0; i < iterations; i++) {
+      const number = 1 + Math.floor(Math.random() * 6);
+      numbers.push(number);
+      if (number === 6) {
+        // Error, se ha sacado un 6
+        return miCallback({
+          error: true,
+          message: "Se ha sacado un 6"
+        });
+      };
+    };
+    // Termina bucle y no se ha sacado 6 
+    return miCallback(null, {
+      error: false,
+      value: numbers
+    });
+  };
+*/
+
+
+
+// Ejecución de la función primaria. Mete el number 10 como 'iterations' y mete una función anónima como parámetro 'miCallback'.
+
+// Si el condicional se deriva al 'return' de 'miCallback' con la 1ª posición de los parámetros ocupada, se tomara esta (en la declaración de la función se le llama a este parámetro 'err')
+
+// Si el condicional se deriva al 'return' de 'miCallback' con la 1ª posición paramétrica como 'null', tomará la 2ª posición (se ha nombrado 'result')
+
+// Entonces los parámetros de la función anónima (referida paramétricamente como miCallback), se han nombrado como 'err' y 'result', y son:
+    // El objeto error, con una propiedad booleana de error == true y otra de mensaje de error
+    // El objeto resultado, con una propiedad booleana de error == false y otra con el array de tiradas
+
+/*
+doTask(10, function(err, result) {
+  if (err) {
+    return console.error(err.message);
+  }
+  return console.log("Tiradas correctas: ", result.value);
+});
+*/
+
+
+
+
+
+console.log("");
+//////////////// ASINCRONÍA CON PROMISES ///////////////
+
+
+/*
+const doTask3 = (iterations) => new Promise((resolve, reject) => {
+    const numbers = [];
+    for (let i = 0; i < iterations; i++) {
+      const number = 1 + Math.floor(Math.random() * 6);
+      numbers.push(number);
+      if (number === 6) {
+        reject({
+          error: true,
+          message: "Se ha sacado un 6"
+        });
+      }
+    }
+    resolve({
+      error: false,
+      value: numbers
+    });
+  });
+
+
+doTask3(10)
+    .then(result => console.log("doTask3 Fine: ", result.value))
+    .catch(err => console.error("doTask3 err: ", err.message));
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ////////  EJERCICIOS  /////////
 
 
@@ -329,24 +519,20 @@ runButton.addEventListener("click", function(value){
         // Consigue que se imprima por consola el nombre de cada usuario.
 
 
-
-
-
-///////  SIN TERMINAR  ///////
+//////// SIN TERMINAR //////////
 
 let users = ['nacho-sf', 'alejandroereyesb', 'Alenriquez96', 'IvanQGervas', 'JavierEspinosaP'];
 
-let promises = users.map(function (user) {
-  fetch(`https://api.github.com/users/${user}`)
-    .then(res => res.json())
-    .then(dataObj => console.log(dataObj.name, dataObj.html_url, dataObj))
-});
+let prom1 = fetch("https://api.github.com/users/nacho-sf");
+let prom2 = fetch("https://api.github.com/users/alejandroereyesb");
+let prom3 = fetch("https://api.github.com/users/Alenriquez96");
+let prom4 = fetch("https://api.github.com/users/IvanQGervas");
+let prom5 = fetch("https://api.github.com/users/JavierEspinosaP");
 
-console.log(promises);
 
-Promise.all(promises)
-  .then(function(value) {
-    console.info("Promise.all -> TODAS las promesas terminadas", value)}, 
-    function(reason) {
-    console.log("Ups...hubo algún error!", reason)
+Promise.all([prom1, prom2, prom3, prom4, prom5])
+  .then(responses => {
+    responses.forEach(response => {
+      console.log(response.status, response.url);
+    })
   });
